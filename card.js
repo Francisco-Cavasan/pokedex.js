e140979f7824d467520f9038ecfc1784a469045d
Spacewiorblue <francisco.cavasan12@gmail.com>
2021-09-30 23:39:51 -0300

    add pokemon cards and search bar

---
+++ card.js
@@ -0,0 +1,37 @@
+function createCard(name,link,health,attack,defense){
+    return `<div class="card">
+<div class="ds-top"></div>
+<div class="avatar-holder">
+  <img src="${link}" alt="">
+</div>
+<div class="name">
+  <a href="${link}" target="_blank">${name}</a>
+</div>
+<div class="ds-skill">
+  <h6>Skills <i class="fa fa-code" aria-hidden="true"></i></h6>
+  <div class="skill html">
+    <h6><i class="fab fa-html5"></i>Health</h6>
+    <div class="bar bar-health">
+    <label>${health}</label>
+      <progress value="${Math.floor((health/100) * 100)}" max="255"></progress>
+    </div>
+  </div>
+  <div class="skill css">
+    <h6><i class="fab fa-css3-alt"></i> Attack </h6>
+    <div class="bar bar-attack">
+    <label>${attack}</label>
+    <progress value="${Math.floor((attack/100) * 100)}" max="255"></progress>
+    </div>
+  </div>
+  <div class="skill javascript">
+    <h6><i class="fab fa-js"></i> Defense </h6>
+    <div class="bar bar-defense">
+    <label>${defense}</label>
+    <progress value="${Math.floor((defense/100) * 100)}" max="255"></progress>
+    </div>
+  </div>
+</div>
+</div>`
+}
+
+export default createCard
--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+import createCard from "./card.js";
 let pokedex = window.pokedex;
-let list = document.getElementById("poke-list");
+let container = document.getElementById("card-container");
 
 const interval = {
   offset: 0,
-  limit: 10000,
+  limit: 1000000,
 };
+
+async function getInfo(url) {
+  let promise = fetch(url).then((response) => response.json());
+  return promise;
+}
+
 pokedex.getPokemonsList(interval).then(function (response) {
   const pokemons = response.results.map((pokemon) => {
-    let names = pokemon.name
-    return names
-  })
+    return pokemon;
+  });
+
   pokemons.forEach((element) => {
-    console.log(element);
-    let ul = document.createElement("li");
-    ul.innerText = element;
-    list.appendChild(ul);
+    getInfo(element.url).then((a) => {
+      let url = a.sprites.front_default;
+      let health = a.stats[0].base_stat;
+      let attack = a.stats[1].base_stat;
+      let defense = a.stats[2].base_stat;
+      let card = createCard(element.name, url, health, attack, defense);
+      let div = document.createElement("div");
+      div.innerHTML = card;
+      container.appendChild(div);
+    });
   });
 });
